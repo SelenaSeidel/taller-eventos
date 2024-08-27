7ea2737f5ec64d2d6383e39b303f11ad3b5e3717
SelenaSeidel <selenaluisaseidel@gmail.com>
2024-08-27 15:01:53 -0300

    sin propagacion

--- eventos.js
+++ eventos.js
@@ -1,7 +1,14 @@
 function saludo(){
     alert("Hola! Soy el div!");
 };
+function saludoboton(event){
+    alert("Hola!");
+    event.stopPropagation();
+};
+
 document.addEventListener("DOMContentLoaded", function (){
  const div = document.querySelector("div");
+ const btn = document.querySelector("button");
+ btn.addEventListener("click",saludoboton);
  div.addEventListener("click", saludo);
 });
